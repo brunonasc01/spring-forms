3b7fdf4d58a4ec5294990ca0cb26525f20d7c2b8
brunonasc <brunonasc@hotmail.com.br>
2019-04-27 19:32:57 -0300

    Form tags Data Binding

--- src/main/java/br/com/spring/mvc/forms/model/Student.java
+++ src/main/java/br/com/spring/mvc/forms/model/Student.java
@@ -1,12 +1,27 @@
 package br.com.spring.mvc.forms.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
+	private String country;
+	
+	private Map<String, String> countryOptions;
+	
+	private String favoriteLanguage;
+	
+	private String[] operatingSystems;
 	
 	public Student() {
-		
+		countryOptions = new LinkedHashMap<String, String>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("US", "United States");
+		countryOptions.put("IN", "India");
+		countryOptions.put("DE", "Germany");
+				
 	}
 
 	public String getFirstName() {
@@ -23,5 +38,37 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public Map<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public void setCountryOptions(Map<String, String> countryOptions) {
+		this.countryOptions = countryOptions;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
 	}
 }
